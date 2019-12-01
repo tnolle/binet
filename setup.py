06867647639f24a4451d46ea92ff8695ff7895fd
Timo Nolle <timonolle@gmail.com>
2019-12-01 20:35:31 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name='april',
+setup(name='binet',
       version='0.5.0',
-      description='APRIL: Artificial Process Intelligence',
+      description='BINet: Multi-perspective Business Process Anomaly Correction',
       long_description='',
       author='Timo Nolle',
       author_email='timonolle@gmail.com',
-      url='https://github.com/tnolle/april',
+      url='https://github.com/tnolle/binet',
       license='GPL-3.0',
       requires=[],
       packages=find_packages())
